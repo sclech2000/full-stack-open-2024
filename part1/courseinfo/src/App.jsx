972619d8903a60ded6df216b92012103171e56eb
sclech2000 <135041166+sclech2000@users.noreply.github.com>
2024-01-09 10:16:37 +0000

    part1 work

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,58 @@
+const Header = (header) => {
+  return (<h1>{header.course}</h1>)
+}
+
+const Part = (content) => {
+  return (
+    <p>
+      {content.name} {content.exercise}
+    </p>
+  )
+}
+const Content = (content) => {
+  return (
+    <div>
+      <Part name={content.parts[0].name} exercise={content.parts[0].exercises} />
+      <Part name={content.parts[1].name} exercise={content.parts[1].exercises} />
+      <Part name={content.parts[2].name} exercise={content.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (total) => {
+  return (
+    <p>
+      Number of exercises {total.parts[0].exercises + total.parts[1].exercises + total.parts[2].exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts : [ 
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'Fundamentals of React',
+        exercises: 14,
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
